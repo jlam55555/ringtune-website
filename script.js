b03d39d4017738c72ac73936684c73bc441d5034
Jonathan Lam <jlam55555@gmail.com>
2016-08-27 23:10:57 -0400

    Added test API

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 $(function() {
   "use strict";
+
+  // GLOBAL-ish VARIABLES
   var progressions = {
-    "I": ["V","IV","vi","III","iii"],
-    "i": ["VIb","V","iv"],
-    "ii": ["V","iii"],
+    "I": ["V","V","V","IV","IV","IV","ii","ii","vi","I","III","iii"],
+    "i": ["VIb","VIb","V","iv"],
+    "ii": ["V","V","iii"],
     "III": ["VIIb"],
-    "iii": ["IV","I"],
-    "IV": ["I","V","ii"],
+    "iii": ["IV","IV","I"],
+    "IV": ["V","V","V","V","I","I","I","ii"],
     "iv": ["i","VIIb"],
-    "V": ["vi","I","i","IV"],
-    "vi": ["IV","iii"],
+    "V": ["I","I","I","I","I","vi","vi","i","IV"],
+    "vi": ["IV","IV","iii"],
     "VIb": ["iv","III"],
     "VIIb": ["i"]
   };
-  var playChord = function(note) {
-    new Audio("res/sound/" + note + ".mp3").play();
-  };
-  var getLength = function() {
-    return 60000/parseInt($("#tempoRange").val());
-  };
+  var playChord = function(note) { new Audio("res/sound/" + note + ".mp3").play(); };
+  var getLength = function() { return 60000/parseInt($("#tempoRange").val()); };
   var used = [];
   var current = "I";
+
+  // HANDLE CLICK OF OPTIONS
   $("#optionsBox").on("click", "img.option", function() {
     current = $(this).data("value");
     playChord(current);
     $("#optionsBox").empty();
-    for(var value of progressions[current])
+    for(var value of progressions[current].filter(function(value, index, array) {
+      return index == 0 || value != array[index-1];
+    }))
       $("#optionsBox").append("<img data-value='" + value + "' class='option' src='res/icons/" + value + ".png'>");
     used.push(current);
-    $("#usedNotes").prepend("<img class='usedNote' src='res/icons/" + current + ".png'>");
+    $("#usedNotes").prepend("<img class='usedNote' data-value='" + current + "' src='res/icons/" + current + ".png'>");
   });
+
+  // PLAY AND STOP THE MUSIC
   var playTimeouts = [];
   $("#playButton").click(function() {
     if(playTimeouts.length > 0) $("#stopButton").click();
@@ -42,6 +46,8 @@ $(function() {
       clearTimeout(i);
     playTimeouts = [];
   }); 
+
+  // RANDOMLY SELECT OPTIONS
   $("#randomButton").click(function() {
     for(let i = 0; i < 10; i++) {
       if(used.length == 0) {
@@ -55,6 +61,8 @@ $(function() {
       }, i*getLength());
     }
   });
+
+  // CLEAR BUTTONS
   $("#closeButton").click(function() {
     if(confirm("Are you sure you want to delete your current RingTune?")) {
       used = [];
@@ -63,7 +71,34 @@ $(function() {
       $("div#optionsBox").html("<img class='option' data-value='I' src='res/icons/I.png'>");
     }
   });
+
+  // TEMPO CONTROL
   $("#tempoRange").on("input", function() {
     $("#tempoValue").text($(this).val());
   });
+
+  // BACKGROUND IMAGE REPOSITIONING
+  $(window).resize(function() {
+    $("img#backgroundLogo").css("top", ($(window).height()-$("img#backgroundLogo").height())/2+"px");
+  }).resize();
+
+  // PLAY NOTE IF CLICKED ON (USED LIST)
+  $(document).on("click", "img.usedNote", function() {
+    playChord($(this).data("value"));    
+  });
+
+  // ATTEMPT TO USE THE API
+  $.ajax({
+    url: "https://api.hooktheory.com/v1/trends/nodes?cp=4,2,4",
+    type: "GET",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorizaton", "Bearer 7008700b22ddff662480256fb05e79c1");
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("Content-Type", "application/json");
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
 });
